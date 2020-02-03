188a6766e776b6a4c3ccf3cb14c069ac091b4fed
Nbenson0911 <nbenson0911@gmail.com>
2020-02-02 20:10:12 -0800

    forum

--- src/components/reply.component.js
+++ src/components/reply.component.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class DisplayBlog extends Component {
+export default class Reply extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -29,23 +31,109 @@ export default class DisplayBlog extends Component {
         console.log(error);
       });
   }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const reply = {
+      username: this.state.username,
+      description: this.state.description,
+      date: this.state.date
+    };
+
+    console.log(reply);
+
+    axios
+      .post("http://localhost:5000/replies/add", reply)
+      .then(res => console.log(res.data));
+
+    window.location = "/forum";
+  }
   render() {
-    console.log(this.state.post.description);
     return (
       <div>
-        <p>
-          <h3>Posted By:</h3>
-        </p>
-        <h2>{this.state.post.username}</h2>
-        <br></br>
-        <p>
-          <h3>Original Question</h3>
-          <em>
-            <h2>{this.state.post.description}</h2>
-          </em>
-          <br />
-        </p>
-        <h3>Reply:</h3> <textarea></textarea>
+        <div>
+          <p>
+            <h3>Posted By:</h3>
+          </p>
+          <h2>{this.state.post.username}</h2>
+          <br></br>
+          <p>
+            <h3>Original Question</h3>
+            <em>
+              <h2>{this.state.post.description}</h2>
+            </em>
+            <br />
+          </p>
+          <h3>Reply:</h3> <textarea></textarea>
+        </div>
+        {/* Reply */}
+        <div>
+          <h3>Reply!</h3>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label>Username: </label>
+              <select
+                ref="userInput"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+              >
+                {this.state.users.map(function(user) {
+                  return (
+                    <option key={user} value={user}>
+                      {user}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className="form-group">
+              <label>Description:</label>
+              <textarea
+                className="form-control"
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+              ></textarea>
+            </div>
+            <div className="form-group">
+              <label>Date:</label>
+              <div>
+                <DatePicker
+                  selected={this.state.date}
+                  onChange={this.onChangeDate}
+                />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <input
+                type="submit"
+                value="Ask Your Question!"
+                className="btn btn-primary"
+              />
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
